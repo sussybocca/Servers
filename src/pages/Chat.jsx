7de10abc398c79871b7da4a2fa0d18e7d26499c9
sussybocca <babyyodacutefry@gmail.com>
2025-10-29 08:57:21 -0500

    Update Chat.jsx

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,1 +1,20 @@
+import { useParams } from "react-router-dom";
+import ChatBox from "../components/ChatBox";
+import { useState, useEffect } from "react";
+import { supabase } from "../lib/supabaseClient";
 
+export default function Chat() {
+  const { roomId } = useParams();
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      const { data } = await supabase.auth.getUser();
+      setUser(data.user);
+    };
+    fetchUser();
+  }, []);
+
+  if (!user) return <div>Loading...</div>;
+  return <ChatBox roomId={roomId} user={user} />;
+}
