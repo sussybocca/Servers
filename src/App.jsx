1686c48b386db62b0b419044531ef8c2315163d8
sussybocca <babyyodacutefry@gmail.com>
2025-10-29 08:54:48 -0500

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,21 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+import ServerPage from "./pages/Server";
+import Chat from "./pages/Chat";
+import PostsPage from "./pages/Posts";
 
+export default function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/server/:id" element={<ServerPage />} />
+        <Route path="/chat/:roomId" element={<Chat />} />
+        <Route path="/posts" element={<PostsPage />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
